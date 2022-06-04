45598fcb52ca92fc12649ffe5e37cfbc737d94dc
Vladislav Belko <vladbelko2002@gmail.com>
2022-06-04 14:36:30 +0300

    Hotfix 2

--- petshop/petshop/settings.py
+++ petshop/petshop/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("POSTGRES_DB"),
         'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRGES_PASSWORD"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("POSTGRES_HOST"),
         'PORT': os.environ.get("POSTGRES_PORT"),
     }
